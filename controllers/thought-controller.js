07ac3f71653ec876eef191016d6e2f389b24af59
bcgosselin <bcgosselin@gmail.com>
2024-05-24 19:19:01 -0500

    added thought-route and outline for thought-controller

---
+++ controllers/thought-controller.js
@@ -0,0 +1,11 @@
+const { User, Thought } = require('../models');
+
+const thoughtController = {
+    getThoughts{},
+    getThoughtById{},
+    createThought{},
+    updateThought{},
+    deleteThought{},
+    addReaction{},
+    removeReaction{},
+}
---
+++ routes/thought-route.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  addReaction,
+  removeReaction,
+} = require('../controllers/thought-controller');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction).delete(removeReaction);
+
+module.exports = router;
